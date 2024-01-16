160f024e144b1ca0a2d2dcb98c3c8e60ba8d1150
Steven Zheng <szheng.persevere@gmail.com>
2024-01-16 10:57:36 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let paragraph = document.querySelector("p")
+paragraph.style.color = "aqua"
+let heading = document.querySelector("h1")
+heading.style.fontsize = "15px"
+paragraph.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id urna a libero interdum posuere. Donec vitae aliquet sapien. Vivamus sagittis erat a lacus tincidunt, vel feugiat odio ultrices."
+let listitem = document.querySelectorAll("li")
+
+listitem[12].style.opacity = ".3"
+listitem[2].innerHTML = "Greetings!"
+let image = document.querySelectorAll("img")
+image[1].innerHTML = "https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc="
+image[1].style.height = "250px"
+
+
+listitem.innerHTML = "<li id="item-17">17</li>"
